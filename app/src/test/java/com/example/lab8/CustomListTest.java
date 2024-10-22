fd39cc7eedcbaf09411da9fd4588672d58a3fb08
Griffin Graham-Bennett <griffgb02@gmail.com>
2024-10-22 17:34:11 -0600

    fail on purpose

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,17 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
-
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size
+     plus one
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 }
